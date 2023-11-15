6fc7a782b3e1bf366c530ca7f514f3a9df8baa9e
4kdrakon <Uzbcordinalochilov@gmail.com>
2023-11-15 20:32:19 +0500

    ikki

---
+++ main/main.js
@@ -0,0 +1,9 @@
+var elHeading = document.querySelector(".heading")
+var UserName = prompt("Mablag' yozing...")
+var Pul = 5959153.6
+
+if(Pul <= UserName) {
+    elHeading.textContent = "alisher borolmaysiz sizda mablag' yetmaydi"
+}else {
+    elHeading.textContent = "Oq yo'l!"
+}
